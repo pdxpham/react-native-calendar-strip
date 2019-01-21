fd815381d2f17ac7876d90234b7a174c66f5b9e2
Quang PHAM <46787469+pdxpham@users.noreply.github.com>
2019-01-21 16:58:58 +0100

    Update CalendarStrip.js

--- src/CalendarStrip.js
+++ src/CalendarStrip.js
@@ -8,6 +8,8 @@ import {polyfill} from 'react-lifecycles-compat';
 import { View, Animated, Easing } from "react-native";
 
 import moment from "moment";
+import 'moment/locale/fr';
+import 'moment/locale/en';
 
 import CalendarHeader from "./CalendarHeader";
 import CalendarDay from "./CalendarDay";
@@ -104,7 +106,7 @@ class CalendarStrip extends Component {
 
     if (props.locale) {
       if (props.locale.name && props.locale.config) {
-        moment.updateLocale(props.locale.name, props.locale.config);
+        moment.locale(props.locale.name);
       } else {
         throw new Error(
           "Locale prop is not in the correct format. \b Locale has to be in form of object, with params NAME and CONFIG!"
